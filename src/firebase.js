be732ba746b7ea90171df786e4b66367513c9ea2
Thomas Knutsen <knutsenthomas@gmail.com>
2026-08-06 09:40:34 +0200

    chore: update fallback firebase configuration to point to his-kingdom-ministry

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || atob("QUl6YVN5RFV2ODZnVll0TE0zN1BKMi1GYWI4ckhYM0VlU2pGUXBj"),
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "his-kingdom-prophets.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "his-kingdom-prophets",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "his-kingdom-prophets.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "973892579910",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:973892579910:web:967aa3d67d2b73efea1047",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-03629NPBTY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || atob("QUl6YVN5QWVsVnNablRVNXhqUXNqZXdXRzdSallFc1FTSEgtYmtF"),
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "his-kingdom-ministry.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "his-kingdom-ministry",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "his-kingdom-ministry.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "791237361706",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:791237361706:web:63516ba3d74436f23ac353",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-28GVKTMCZE"
 };
 
 const app = initializeApp(firebaseConfig);
